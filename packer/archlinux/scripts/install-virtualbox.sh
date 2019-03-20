dc1dfbdc7f68b8702748be488549e518b123f893
Sophia Hudson <sophia-hudson@pluralsight.com>
2019-03-19 21:11:52 -0600

    Reformat the file for nicer reading

--- packer/archlinux/scripts/install-virtualbox.sh
+++ packer/archlinux/scripts/install-virtualbox.sh
@@ -2,11 +2,14 @@
 
 # VirtualBox Guest Additions
 # https://wiki.archlinux.org/index.php/VirtualBox
-/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-modules-arch nfs-utils
+/usr/bin/pacman -S --noconfirm linux-headers \
+                               virtualbox-guest-utils \
+                               virtualbox-guest-modules-arch \
+                               nfs-utils
 echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
 
 /usr/bin/systemctl enable vboxservice.service
 /usr/bin/systemctl enable rpcbind.service
 
 # Add groups for VirtualBox folder sharing
-/usr/bin/usermod --append --groups vagrant,vboxsf,vboxusers vagrant
+/usr/bin/usermod --append --groups vagrant,vboxsf vagrant
