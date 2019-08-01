2a8d475e2ebdfd8a0ea96dfb6043d7eac4c81f81
Sophia Hudson <sophia-hudson@pluralsight.com>
2019-08-01 17:38:13 -0600

    Add gitmodules and update opensc install script

--- scripts/install-opensc.sh
+++ scripts/install-opensc.sh
@@ -7,5 +7,9 @@
                                pcsc-tools
 
 cat /tmp/etc/opensc.conf > /etc/opensc.conf
+echo -e 'enable_pinpad = false' >> /etc/opensc.conf
+echo -e 'card_drivers = cac' >> /etc/opensc.conf
+echo -e 'force_card_driver = cac' >> /etc/opensc.conf
 
 /usr/bin/systemctl enable pcscd.service
+/usr/bin/systemctl enable pcscd.socket
